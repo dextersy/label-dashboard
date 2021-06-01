653226c4180cdbd7f31252f150f0a03ba33c843c
Dexter Sy <sy.dexter@gmail.com>
2021-06-01 08:30:21 +0800

    Initial dashboard implementation

---
+++ src/inc/view/dashboard/album-table.php
@@ -0,0 +1,40 @@
+<?
+    session_start();
+    include_once('./inc/controller/get-artist-list.php');
+    include_once('./inc/controller/get-release-list.php');
+
+    if ($isAdmin) {
+        $artists = getAllArtists();
+    }
+    else {
+        $artists = getArtistListForUser($_SESSION['logged_in_user']);
+    }
+?>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr><th>Catalog Number</th>
+            <th>Artist</th>
+            <th>Title</th>
+        </thead>
+        <tbody>
+<? 
+    if ($artists) {
+        foreach ($artists as $artist) {
+            $releases = getReleaseListForArtist($artist->id);
+            if ($releases) {
+                foreach ($releases as $release) { 
+?>
+            <tr>
+                <td><?=$release->catalog_no;?></td>
+                <td><?=$artist->name;?></td>
+                <td><?=$release->title;?></td>
+            </tr>
+<?              }
+            }
+        } 
+    }
+?>
+        </tbody>
+    </table>
+</div>
