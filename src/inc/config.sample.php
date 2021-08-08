ebc33bcd72279aa3760c38f6090da2c11349c380
Dexter Sy <dexter@pop-os.localdomain>
2021-08-08 15:24:57 +0800

    Change config file to template and add actual config to ignore list

---
+++ src/inc/config.sample.php
@@ -0,0 +1,18 @@
+<?php
+// ONCE YOU'RE DONE UPDATING, PLEASE RENAME THIS FILE TO CONFIG.PHP 
+// TO REFLECT IN ACTUAL SERVER
+
+// Update your actual server, database, user, and password here
+define('DB_SERVER', 'localhost');
+define('DB_DATABASE', 'meltrecords_dashboard');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+
+// Update your actual SMTP configuration here
+define('SMTP_HOST', 'email-smtp.ap-southeast-1.amazonaws.com');
+define('SMTP_PORT', '587');
+define('SMTP_SECURE', 'tls');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
+
+?>
