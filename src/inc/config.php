8d561205362a9c12f3460dcc9e65669eb6415fe5
Dexter Sy <dexter@pop-os.localdomain>
2021-08-08 15:24:57 +0800

    Change config file to template and add actual config to ignore list

---
+++ src/inc/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_DATABASE', 'record_label');
+define('DB_USER', 'recordlabeluser');
+define('DB_PASSWORD', 'RecordLabelPass1!');
+
+// SMTP configuration
+define('SMTP_HOST', 'email-smtp.ap-southeast-1.amazonaws.com');
+define('SMTP_PORT', '587');
+define('SMTP_SECURE', 'tls');
+define('SMTP_USER', 'AKIAVZIDMMHVQWBYZPBL');
+define('SMTP_PASS', 'BHgucHEQ9fjbkyzIosyIxXZa7hLokX/OgTHhk157CNyq');
+
+?>
