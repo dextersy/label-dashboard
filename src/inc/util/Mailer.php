63923a1aaecc2e9cdce89e04d075074b7e7a956e
Dexter Sy <sy.dexter@gmail.com>
2021-08-12 13:33:29 +0800

    Refactor mail to a utility

---
+++ src/inc/util/Mailer.php
@@ -0,0 +1,42 @@
+<?
+    use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+    // Load Composer's autoloader
+	require 'vendor/autoload.php';
+
+    function sendEmail($emailAddress, $subject, $body) {
+		try {
+			//PHPMailer Object
+			$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+			$mail->isSMTP();
+			$mail->Host = SMTP_HOST;
+			$mail->SMTPAuth = true;
+			$mail->Username = SMTP_USER;
+			$mail->Password = SMTP_PASS;
+			$mail->Port = SMTP_PORT;
+
+			//From email address and name
+			$mail->From = "no-reply@melt-records.com";
+			$mail->FromName = "Melt Records Artist Dashboard";
+
+			//To address and name
+			$mail->addAddress($emailAddress, "");
+
+			//Address to which recipient will reply
+			$mail->addReplyTo("hi@melt-records.com", "Reply");
+
+			//Send HTML or Plain Text email
+			$mail->isHTML(true);
+
+			$mail->Subject = $subject;
+			$mail->Body = $body;
+			$mail->AltBody = "This is the plain text version of the email content";
+
+			$mail->send();
+		} catch (Exception $e) {
+			return false;
+		}
+		return true;
+	}
+?>
