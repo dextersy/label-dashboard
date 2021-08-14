d1441923b2ab2ca51a98f5cc5a4fabcd260e039e
Dexter Sy <sy.dexter@gmail.com>
2021-08-14 12:43:43 +0800

    Add mailer for new earnings

--- src/action.add-earning.php
+++ src/action.add-earning.php
@@ -1,12 +1,15 @@
 <?php
     require_once('./inc/model/earning.php');
     require_once('./inc/model/royalty.php');
+    require_once('./inc/model/release.php');
     require_once('./inc/model/releaseartist.php');
     require_once('./inc/model/recuperableexpense.php');
     require_once('./inc/util/Redirect.php');
     require_once('./inc/controller/get-artist-list.php');
     require_once('./inc/controller/get-recuperable-expense.php');
     require_once('./inc/controller/access_check.php');
+    require_once('./inc/controller/get_team_members.php');
+    require_once('./inc/util/Mailer.php');
 
     if(!$isAdmin) {
         redirectTo("/index.php");
@@ -16,27 +19,28 @@
     $earning = new Earning;
     $earning->fromFormPOST($_POST);
     $earning->save();
-    
-    // Calculate royalties
-    if ( $_POST['calculateRoyalties'] == '1') {
-        // Recup amount
-        $recuperableExpenseBalance = getRecuperableExpenseBalance($_POST['release_id']);
-        if ($earning->amount >= $recuperableExpenseBalance) {
-            $recuperatedAmount = $recuperableExpenseBalance;
-        }
-        else {
-            $recuperatedAmount = $earning->amount;
-        }
-        $earning->amount -= $recuperatedAmount; 
-        $recuperableExpense = new RecuperableExpense(null, $_POST['release_id'], "Recouped from earnings", $recuperatedAmount*-1);
-        $recuperableExpense->save();
 
-        // Calculate royalties per artist
-        if ($earning->amount > 0) {
-            $artists = getArtistListForRelease($_POST['release_id']);
-        }
-        if ($artists) {
-            foreach ($artists as $artist) {
+    // Recup amount
+    $recuperableExpenseBalance = getRecuperableExpenseBalance($_POST['release_id']);
+    if ($earning->amount >= $recuperableExpenseBalance) {
+        $recuperatedAmount = $recuperableExpenseBalance;
+    }
+    else {
+        $recuperatedAmount = $earning->amount;
+    }
+    $earningRemainingForRoyalties = $earning->amount - $recuperatedAmount; 
+    $recuperableExpense = new RecuperableExpense(null, $_POST['release_id'], "Recouped from earnings", $recuperatedAmount*-1);
+    $recuperableExpense->save();
+   
+    $release = new Release;
+    $release->fromID($_POST['release_id']);
+    $artists = getArtistListForRelease($_POST['release_id']);
+    
+    if ($artists) {
+        foreach ($artists as $artist) {
+            unset($royalty);
+            // Calculate royalties
+            if ( $_POST['calculateRoyalties'] == '1') {
                 $releaseArtist = new ReleaseArtist;
                 $releaseArtist->fromID($artist->id, $_POST['release_id']);
                 switch($_POST['type']) {
@@ -60,7 +64,7 @@
                 }
                 
                 if ($royaltyBase == 'Revenue') {
-                    $royaltyAmount = $earning->amount * $royaltyPercent;
+                    $royaltyAmount = $earningRemainingForRoyalties * $royaltyPercent;
                 }
 
                 $royalty = new Royalty;
@@ -70,26 +74,55 @@
                 $royalty->amount = $royaltyAmount;
                 $royalty->release_id = $_POST['release_id'];
                 $royalty->description = "Royalties from " . $_POST['description'];
-                
                 $royalty->save();
+            }
+
+            // Send email notification
+            $users = getTeamMembersForArtist($artist->id);
+            foreach ($users as $user) {
+                sendEarningsNotification(
+                        $user->email_address, 
+                        $artist, 
+                        $release, 
+                        $earning, 
+                        $recuperatedAmount,
+                        $royalty);
             }
         }
     }
 
-    function sendEarningsNotification($emailAddress, $artistName, $inviteHash) {
-		$subject = "You've been invited to join ". $artistName . "'s team!";
-		return sendEmail($emailAddress, $subject, generateEmailFromTemplate($artistName, $inviteHash));
+    function sendEarningsNotification ( $emailAddress, $artist, $release, $earning, $recuperatedAmount, $royalty) 
+    {
+		$subject = "New earnings posted for ". $artist->name . " - " . $release->title;
+		return sendEmail($emailAddress, $subject, 
+                    generateEmailFromTemplate(
+                        $artist->name, 
+                        $release->title,
+                        $earning->description,
+                        number_format($earning->amount, 2),
+                        number_format($recuperatedAmount, 2),
+                        number_format(getRecuperableExpenseBalance($release->id), 2),
+                        isset($royalty)? number_format($royalty->amount, 2) : null
+                        )
+                    );
 	}
 
-    function generateEmailFromTemplate($artistName, $inviteHash) {
-		define ('TEMPLATE_LOCATION', 'assets/templates/invite_email.html', false);
+    function generateEmailFromTemplate($artistName, $releaseName, $earningDescription, 
+            $earningAmount, $recuperatedAmount, $recuperableExpenseBalance, $royaltyAmount) {
+		define ('TEMPLATE_LOCATION', 'assets/templates/earning_notification.html', false);
 		$file = fopen(TEMPLATE_LOCATION, 'r');
 		$msg = fread($file, filesize(TEMPLATE_LOCATION));
 		fclose($file);
 
         $msg = str_replace("%LOGO%", getProtocol() . $_SERVER['HTTP_HOST'] . "/assets/img/logo-purple.png", $msg);
-		$msg = str_replace('%ARTIST%', $artistName, $msg);
-		$msg = str_replace('%LINK%', getProtocol() . $_SERVER['HTTP_HOST'] . "/setprofile.php?u=" . $inviteHash, $msg);
+		$msg = str_replace('%ARTIST_NAME%', $artistName, $msg);
+        $msg = str_replace('%RELEASE_TITLE%', $releaseName, $msg);
+        $msg = str_replace('%EARNING_DESC%', $earningDescription, $msg);
+        $msg = str_replace('%EARNING_AMOUNT%', $earningAmount, $msg);
+        $msg = str_replace('%RECUPERATED_EXPENSE%', $recuperatedAmount, $msg);
+        $msg = str_replace('%RECUPERABLE_BALANCE%', $recuperableExpenseBalance, $msg);
+        $msg = str_replace('%ROYALTY%', $royaltyAmount!=null? $royaltyAmount: "(Not applied)", $msg);
+		$msg = str_replace('%URL%', "https://artists.melt-records.com", $msg);
 		
 		return $msg;
 	}
