eee3949f8ea91e6c4b42fe14ebf4959b97d69c0f
Dexter Sy <dexter@pop-os.localdomain>
2021-08-17 20:19:13 +0800

    Date recorded for recuperable expense should be NULL by default

--- schema/migration.sql
+++ schema/migration.sql
@@ -1,2 +1,3 @@
 -- Updated August 17, 2021
 ALTER TABLE `recuperable_expense` ADD `date_recorded` DATE NULL DEFAULT '2021-08-17' AFTER `expense_amount`;
+ALTER TABLE `recuperable_expense` CHANGE `date_recorded` `date_recorded` DATE NULL DEFAULT NULL;
