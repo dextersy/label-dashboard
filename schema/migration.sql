30aca3e97f83030253e9a826d6152a4ee6c2522e
Dexter Sy <sy.dexter@gmail.com>
2021-08-23 14:41:02 +0800

    Add date marker on migration script

--- schema/migration.sql
+++ schema/migration.sql
@@ -1,6 +1,8 @@
 -- Updated August 17, 2021
 ALTER TABLE `recuperable_expense` ADD `date_recorded` DATE NULL DEFAULT '2021-08-17' AFTER `expense_amount`;
 ALTER TABLE `recuperable_expense` CHANGE `date_recorded` `date_recorded` DATE NULL DEFAULT NULL;
+
+-- Uploaded August 23, 2021
 ALTER TABLE `artist` 
 DROP COLUMN `starting_balance`,
 ADD COLUMN `profile_photo` VARCHAR(255) NULL AFTER `website_page_url`;
