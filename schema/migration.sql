e72a33567d6f4ec77be4d3b9f5bfaef7b5f0a948
Dexter Sy <sy.dexter@gmail.com>
2021-08-23 14:40:45 +0800

    Remove database name from migration script

--- schema/migration.sql
+++ schema/migration.sql
@@ -1,6 +1,6 @@
 -- Updated August 17, 2021
 ALTER TABLE `recuperable_expense` ADD `date_recorded` DATE NULL DEFAULT '2021-08-17' AFTER `expense_amount`;
 ALTER TABLE `recuperable_expense` CHANGE `date_recorded` `date_recorded` DATE NULL DEFAULT NULL;
-ALTER TABLE `record_label`.`artist` 
+ALTER TABLE `artist` 
 DROP COLUMN `starting_balance`,
 ADD COLUMN `profile_photo` VARCHAR(255) NULL AFTER `website_page_url`;
