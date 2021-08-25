c2fd9b6854e7ae3f86e1682d488bf8cc35efdfea
Dexter Sy <sy.dexter@gmail.com>
2021-08-25 17:07:46 +0800

    Layout adjustments to profile tab

--- src/inc/view/artists/artist-profile-tab.php
+++ src/inc/view/artists/artist-profile-tab.php
@@ -17,14 +17,14 @@ $formAction = "action.update-artist.php?from=" . $_SERVER['REQUEST_URI'];
 <form action="<?=$formAction;?>" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="id" value="<?=$artist->id;?>">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <img src="<?=($artist->profile_photo!="") ? $artist->profile_photo : "assets/img/placeholder.jpg";?>" width="100%" style="border:#cccccc 1px solid; padding:5px;">
             <div class="form-group">
                 <label for="profile_photo">Profile Photo</label>
                 <input type="file" class="form-control" id="profile_photo" name="profile_photo" accept=".jpg, .png" />
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <div class="form-group">
                 <label for="name">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Artist Name" value="<?=$artist->name;?>">
@@ -44,10 +44,14 @@ $formAction = "action.update-artist.php?from=" . $_SERVER['REQUEST_URI'];
             <div class="form-group">
                 <label for="twitterHandle">Twitter <i class="fa fa-twitter" aria-hidden="true"></i></label>
                 <input type="text" class="form-control" id="twitterHandle" name="twitterHandle" placeholder="Twitter handle" value="<?=$artist->twitter_handle;?>">
-            </div>     
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
             <div class="form-group">
                 <label for="bio">Bio</label>
-                <textarea class="form-control" id="bio" name="bio"><?=$artist->bio;?></textarea>
+                <textarea class="form-control" id="bio" name="bio" style="height:250px;"><?=$artist->bio;?></textarea>
             </div>                                                            
             <button type="submit" class="btn btn-default">Save Changes</button>
         </div>
