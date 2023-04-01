3d40ed704616ed53e4b9321c44c089c1ee7ca05f
Dexter Sy <sy.dexter@gmail.com>
2023-04-01 22:14:16 +0800

    Create Paymongo payment link

--- src/public/tickets/action.buy.php
+++ src/public/tickets/action.buy.php
@@ -1,21 +1,69 @@
 <?
     chdir("../..");
     include_once("./inc/model/ticket.php");
+    include_once("./inc/model/event.php");
     include_once("./inc/util/Redirect.php");
+    include_once("./inc/config.php");
+
+    function createPaymentLink($amount, $description) {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+                CURLOPT_URL => "https://api.paymongo.com/v1/links",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "{\"data\":{\"attributes\":{\"amount\":". $amount*100 . ",\"description\":\"" . $description . "\"}}}",
+                CURLOPT_HTTPHEADER => [
+                    "accept: application/json",
+                    "authorization: Basic " . PAYMONGO_SECRET_KEY,
+                    "content-type: application/json"
+                ],
+            ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return $err;
+        } else {
+            return json_decode($response, false);
+        }
+    }
 
     $ticket = new Ticket;
     $ticket->fromFormPOST($_POST);
     $ticket->status = "New";
-     // Generate ticket code
-     $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-     $randomString = '';
-     for ($i = 0; $i < 5; $i++) {
-         $randomString .= $characters[rand(0, strlen($characters) - 1)];
-     }
-     $ticket->ticket_code = $randomString;
+    // Generate ticket code
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < 5; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    $ticket->ticket_code = $randomString;
     $ticket->save();
 
-    // todo Generate payment link
+    $event = new Event;
+    $event->fromID($ticket->event_id);
+
+    $amount = $event->ticket_price * $ticket->number_of_entries;
+    $description = "Payment for " . $event->title . " - Ticket # " . $ticket->id;
+    $response = createPaymentLink($amount, $description);
+
+    if(isset($response->data->attributes)) {
+        $ticket->payment_link = $response->data->attributes->checkout_url;
+        $ticket->payment_link_id = $response->data->id;
+        $ticket->save();
+    }
+    else {
+        echo $response;
+    }
 
     // todo Send notification
 
--- src/public/tickets/pay.php
+++ src/public/tickets/pay.php
@@ -17,39 +17,33 @@
   <title>Buy tickets to <?=$event->title;?></title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-KQHJ23P');</script>
-  <!-- End Google Tag Manager -->
 </header>
 
 <body>
-  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQHJ23P"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <link href="style.css" rel="stylesheet">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 
-<div class="wrapper fadeInDown">
+<div class="wrapper">
     <div id="formHeader">
+    <img src="../../<?=$event->poster_url;?>" width="30%">
     <h1 style="color:black;">Thank you for your order!</strong></h1>
-    <p>Your tickets are reserved. Please continue to the payment link below to finish your order.</p>
+    <p>Your tickets to <strong><?=$event->title;?></strong> are reserved. Please continue to the payment link below to finish your order.</p>
 </div>
   <div id="formContent">
     <p>The total amount for your order is </p>
     <h1><?=money_format("Php %i", $ticket->number_of_entries * $event->ticket_price)?></h1>
     <!-- Tabs Titles -->
     <!-- Login Form -->
-      <input type="button" value="Pay for Tickets">
+    <a href="<?=$ticket->payment_link;?>" target="_blank">
+      <button class="paymentLinkButton">Pay for Tickets</button>
+    </a>
+    <p style="font-size:12px"><em>This link has also been sent to your email.</em></p>
     <!-- Remind Passowrd -->
     <div id="formFooter">
+    <p style="font-size:12px">This link will redirect you to our external payment provider.</p>
     <p style="font-size:12px;">Upon completing your payment, please give us 2 to 3 days to verify and send your ticket.</p>
     </div>
   </div>
