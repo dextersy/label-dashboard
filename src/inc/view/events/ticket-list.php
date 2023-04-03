6051f018d8042d30d9186cd4d99855911bc8e208
Dexter Sy <sy.dexter@gmail.com>
2023-04-04 07:23:02 +0800

    Fix mark paid link

--- src/inc/view/events/ticket-list.php
+++ src/inc/view/events/ticket-list.php
@@ -26,7 +26,7 @@
             return "";
         }
         if ( $status == "New" ) {
-            $link = "<a href=\"action.mark-tickets-paid.php?ticket_id=" . $id . "\">[ Mark as paid ]</a>";
+            $link = "<a href=\"action.mark-ticket-paid.php?ticket_id=" . $id . "\">[ Mark as paid ]</a>";
         }
         else if ($status == "Payment Confirmed") {
             $link = "<a href=\"action.send-tickets.php?ticket_id=" . $id . "\">[ Send Ticket ]</a>";
