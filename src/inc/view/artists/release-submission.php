e8c44edc256a1df9163dccd089583153cacc7c7f
Dexter Sy <sy.dexter@gmail.com>
2024-01-09 10:16:27 +0800

    Use brand setting for release submission URL

--- src/inc/view/artists/release-submission.php
+++ src/inc/view/artists/release-submission.php
@@ -1,3 +1,17 @@
 <h3>Submit your release</h3>
 <p>Submit your track information and files to us via Google Forms through the button below. <br>The form will open in a new tab. <br>After receiving your submission, we will create the release for you.</p>
-<p><a href="https://forms.gle/k9FMr8HeJLWQGnXV8" target="_blank"><button class="btn"><i class="fa fa-arrow-right"></i> Go to Form</button></p>
+<?
+    require_once('./inc/model/brand.php');
+    $brand = new Brand();
+    $brand->fromID($_SESSION['brand_id']);
+    if(isset($brand->release_submission_url) && strlen($brand->release_submission_url) > 0 ) {
+?>
+<p><a href="<?=$brand->release_submission_url;?>" target="_blank"><button class="btn"><i class="fa fa-arrow-right"></i> Go to Form</button></p>
+<?
+    }
+    else {
+?>
+<p style="color:red;"><em>No release submission URL set. Please contact your label.</em></p>
+<?
+    }
+?>
