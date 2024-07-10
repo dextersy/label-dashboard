0d59ed72a8d17fd1048aa5141afc15c5012a52f3
Dexter Sy <sy.dexter@gmail.com>
2024-07-10 09:23:28 +0800

    Add opengraph image to Buy page

--- src/public/tickets/buy.php
+++ src/public/tickets/buy.php
@@ -13,7 +13,7 @@
   <title>Buy tickets to <?=$event->title;?></title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
-
+  <meta property="og:image" content="<?=$event->poster_url;?>"/>
 </header>
 
 <body>
