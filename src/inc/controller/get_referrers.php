17c3237bdbf66ceca9a5c265c4dfcdc01c2b4bcb
Dexter Sy <sy.dexter@gmail.com>
2024-08-12 12:05:45 +0800

    Fix wrong gross sales for referrer list

--- src/inc/controller/get_referrers.php
+++ src/inc/controller/get_referrers.php
@@ -50,7 +50,7 @@
         $net_amount_sold = 0;
         while($row = $result->fetch_assoc()) {
             $tickets_sold += $row['number_of_entries'];
-            $gross_amount_sold += $row['price_per_ticket'] * $tickets_sold;
+            $gross_amount_sold += $row['price_per_ticket'] * $row['number_of_entries'];
             $net_amount_sold = $gross_amount_sold - $row['payment_processing_fee'];
         }
         $referrerSales = new EventReferrerSales(
