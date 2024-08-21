63008189b5078a062d22db21398da5c8a5efdbd3
Dexter Sy <sy.dexter@gmail.com>
2024-08-21 09:59:03 +0800

    Include json data in email notif for Paymongo webhook

--- src/public/api/paymongo-webhook-test.php
+++ src/public/api/paymongo-webhook-test.php
@@ -2,10 +2,10 @@
     chdir("../..");
     include_once("./inc/util/Mailer.php");
 
-    function sendAdminNotification() {
+    function sendAdminNotification($jsonData) {
 		$subject = "Paymongo webhook event received.";
 		$emailAddresses[0] = "sy.dexter@gmail.com"; // TODO Replace with actual administrators later
-        $body = "Received a webhook event.";
+        $body = "Received a webhook event.<br>" . $jsonData;
 		return sendEmail($emailAddresses, $subject, $body);
 	}
 
@@ -17,11 +17,13 @@
 	}
 
     $jsonData = file_get_contents('php://input');
+    sendAdminNotification($jsonData);
+
     $data = json_decode($jsonData, true);
     
 
     if ($data !== null) {
-        sendAdminNotification();
+
     } else {
         sendAdminFailureNotification();
     }
