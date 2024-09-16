33374ab2fd1e510c179585ab3fe84c71854610e6
Dexter Sy <sy.dexter@gmail.com>
2024-09-16 09:59:45 +0800

    Improve payout point update email

--- src/action.set-payout-point.php
+++ src/action.set-payout-point.php
@@ -9,7 +9,7 @@
     require_once('./inc/controller/users-controller.php');
 
 
-    function __sendNotification($artist) {
+    function __sendNotification($artist, $brandName, $brandColor, $user) {
         $i = 0;
         
         $admins = getAllAdmins($_SESSION['brand_id']);
@@ -25,11 +25,11 @@
             }
         }
 		$subject = "Payout point for ". $artist->name . " updated.";
-        return sendEmail($emailAddresses, $subject, __generateEmailFromTemplate($artist->name, $artist->payout_point));
+        return sendEmail($emailAddresses, $subject, __generateEmailFromTemplate($artist->name, $artist->payout_point, $brandName, $brandColor, $user->first_name . ' ' . $user->last_name));
 	}
 
 
-    function __generateEmailFromTemplate($artistName, $payoutPoint) {
+    function __generateEmailFromTemplate($artistName, $payoutPoint, $brandName, $brandColor, $memberName) {
 		define ('TEMPLATE_LOCATION', 'assets/templates/artist_update_payout_point.html', false);
 		$file = fopen(TEMPLATE_LOCATION, 'r');
 		$msg = fread($file, filesize(TEMPLATE_LOCATION));
@@ -37,8 +37,11 @@
 
         $msg = str_replace("%LOGO%", getProtocol() . $_SERVER['HTTP_HOST'] . "/" . $_SESSION['brand_logo'], $msg);
 		$msg = str_replace('%ARTIST_NAME%', $artistName, $msg);
-        $msg = str_replace('%PAYOUT_POINT%', $payoutPoint, $msg);
-		$msg = str_replace('%LINK%', getProtocol() . $_SERVER['HTTP_HOST'] . "/financial.php#payments", $msg);
+        $msg = str_replace('%PAYOUT_POINT%', 'Php ' . number_format($payoutPoint, 2, '.', ','), $msg);
+        $msg = str_replace('%BRAND_NAME%', $brandName, $msg);
+        $msg = str_replace('%BRAND_COLOR%', $brandColor, $msg);
+        $msg = str_replace('%MEMBER_NAME%', $memberName, $msg);
+		$msg = str_replace('%URL%', getProtocol() . $_SERVER['HTTP_HOST'] . "/financial.php#payments", $msg);
 		
 		return $msg;
 	}
@@ -48,10 +51,15 @@
     if ($artist->fromID($_POST['id'])) {
         $artist->payout_point = $_POST['payout_point'];
         $result = $artist->save();
+
+        // Get user details for audit trail
+        $user = new User;
+        $user->fromID($_SESSION['logged_in_user']);
+
         if ($result) {
-            __sendNotification($artist);
+            __sendNotification($artist, $_SESSION['brand_name'], $_SESSION['brand_color'], $user);
         }
     }
 
-    redirectTo("/financial.php?action=payoutPoint&status=" . ($result ? "OK" : "Failed"));
+    redirectTo("/financial.php?action=payoutPoint&status=" . ($result ? "OK" : "Failed") . "#payments");
 ?>
