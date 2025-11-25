46c052ccd098f67da0ab06f8f2b3c4b374c90854
dextersy <sy.dexter@gmail.com>
2025-11-25 17:14:43 +0800

    Show name for pending invites if available

--- src_new/label-dashboard-web/src/app/pages/admin/components/users-tab.component.ts
+++ src_new/label-dashboard-web/src/app/pages/admin/components/users-tab.component.ts
@@ -69,8 +69,8 @@ export class UsersTabComponent implements OnInit {
     return users.map(user => ({
       ...user,
       username: user.has_pending_invite ? '(Pending Invite)' : user.username,
-      first_name: user.has_pending_invite ? '-' : (user.first_name || ''),
-      last_name: user.has_pending_invite ? '-' : (user.last_name || '')
+      first_name: user.first_name || '-',
+      last_name: user.last_name || '-'
     }));
   }
 
